f770e7a040c0c2969826b035ca7a955ee00e22de
Darren Moody <darren@paladinarcher.com>
2018-04-26 11:07:26 -0600

    Changes towards getting the ability to change email addresses.

--- imports/ui/pages/user_profile/user_profile.js
+++ imports/ui/pages/user_profile/user_profile.js
@@ -1,5 +1,9 @@
-import { User } from '/imports/api/users/users.js';
+import {
+    User
+} from '/imports/api/users/users.js';
 import './user_profile.html';
+import '../verify/verify.html';
+import '../verify/verify.js';
 
 Template.user_profile.onCreated(function () {
     if (this.data.userId) {
@@ -9,7 +13,7 @@ Template.user_profile.onCreated(function () {
     } else {
         this.userId = Meteor.userId();
     }
-    this.autorun( () => {
+    this.autorun(() => {
         this.subscription = this.subscribe('userData', {
             onStop: function () {
                 console.log("User profile subscription stopped! ", arguments, this);
@@ -30,14 +34,23 @@ Template.user_profile.onCreated(function () {
     });
 });
 Template.user_profile.onRendered(function () {
-    Meteor.setTimeout(function() {
+    Meteor.setTimeout(function () {
         $("#input-bdate").datetimepicker({
-            useCurrent:false,
-            showClear:true,
-            showClose:true,
-            format:'YYYY-MM-DD'
+            useCurrent: false,
+            showClear: true,
+            showClose: true,
+            format: 'YYYY-MM-DD'
         });
     }, 1000);
+    let updateEmailVisibility = function() {
+        let m;
+        m = $(elem).css('background-color').replace(/^rgba?\(|\s+|\)$/g, '').split(',');
+        let btnChange = $(elem).parents('div.change-email-div').find('button.btn-email-change');
+        let emailInput = $(elem).parents('div.change-email-div').find('input.input-new-email');
+        let emailBtnGroupDiv = $(elem).parents('div.change-email-div').find('div.btn-email-group');
+        emailInput.css('visibility', 'visible'); emailInput.css('display', 'unset');
+        emailBtnGroupDiv.css('visibility', 'visible'); emailBtnGroupDiv.css('display', 'unset');
+    }
 });
 
 Template.user_profile.helpers({
@@ -46,29 +59,31 @@ Template.user_profile.helpers({
     },
     userField(fldName) {
         let uid = Template.instance().userId;
-        let u = User.findOne( {_id:uid} );
+        let u = User.findOne({
+            _id: uid
+        });
         if (u) {
             switch (fldName) {
-            case 'firstName':
-                return u.MyProfile.firstName;
-                break;
-            case 'lastName':
-                return u.MyProfile.lastName;
-                break;
-            case 'fullName':
-                return u.MyProfile.fullName();
-                break;
-            case 'gender':
-                return (u.MyProfile.gender ? 'female' : 'male');
-                break;
-            case 'birthDate':
-                let d = u.MyProfile.birthDate;
-                let dateText = new Date(d.getTime() - d.getTimezoneOffset()*60000).toISOString().slice(0,-1);
-                return dateText;
-                break;
-            case 'dashboardPanes':
-                return (u.MyProfile.dashboardPanes.length > 0 ? 'Custom' : 'Default');
-                break;
+                case 'firstName':
+                    return u.MyProfile.firstName;
+                    break;
+                case 'lastName':
+                    return u.MyProfile.lastName;
+                    break;
+                case 'fullName':
+                    return u.MyProfile.fullName();
+                    break;
+                case 'gender':
+                    return (u.MyProfile.gender ? 'female' : 'male');
+                    break;
+                case 'birthDate':
+                    let d = u.MyProfile.birthDate;
+                    let dateText = new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString().slice(0, -1);
+                    return dateText;
+                    break;
+                case 'dashboardPanes':
+                    return (u.MyProfile.dashboardPanes.length > 0 ? 'Custom' : 'Default');
+                    break;
             }
             return u.MyProfile.fullName();
         } else {
@@ -77,16 +92,18 @@ Template.user_profile.helpers({
     },
     genderSelected(label) {
         let uid = Template.instance().userId;
-        let u = User.findOne( {_id:uid} );
+        let u = User.findOne({
+            _id: uid
+        });
         if (!u) return "";
         if (
-            ("f" === label.slice(0,1) || "F" === label.slice(0,1)) &&
+            ("f" === label.slice(0, 1) || "F" === label.slice(0, 1)) &&
             u.MyProfile.gender === true
         ) {
             return "selected";
         }
         if (
-            ("m" === label.slice(0,1) || "M" === label.slice(0,1)) &&
+            ("m" === label.slice(0, 1) || "M" === label.slice(0, 1)) &&
             u.MyProfile.gender === false
         ) {
             return "selected";
@@ -95,7 +112,9 @@ Template.user_profile.helpers({
     },
     userName() {
         let uid = Template.instance().userId;
-        let u = User.findOne( {_id:uid} );
+        let u = User.findOne({
+            _id: uid
+        });
         if (u) {
             return u.MyProfile.fullName();
         } else {
@@ -104,7 +123,9 @@ Template.user_profile.helpers({
     },
     emailAddress() {
         let uid = Template.instance().userId;
-        let u = User.findOne( {_id:uid} );
+        let u = User.findOne({
+            _id: uid
+        });
         if (u) {
             emailAddresses = [];
             for (let i = 0; i < u.emails.length; i++) {
@@ -114,24 +135,26 @@ Template.user_profile.helpers({
         } else {
             return "";
         }
-    }
+    },
+    
 });
 
+
 Template.user_profile.events({
-    'change input.flat,textarea.flat,select'(event, instance) {
+    'change input.flat,textarea.flat,select' (event, instance) {
         $(event.target).addClass('changed');
         $("#btn-group").fadeIn();
     },
-    'dp.change'(event, instance) {
+    'dp.change' (event, instance) {
         $(event.target).addClass('changed');
         $("#btn-group").fadeIn();
     },
-    'keyup input,textarea'(event, instance) {
+    'keyup input,textarea' (event, instance) {
         let $t = $(event.target);
         $t.addClass('changed');
-        $("#btn-group").fadeIn( );
+        $("#btn-group").fadeIn();
     },
-    'click button.btn-save'(event, instance) {
+    'click button.btn-save' (event, instance) {
         let $t = $(event.target);
         $t.closest(".container").find(".changed").removeClass("changed");
         //todo: update database
@@ -142,14 +165,40 @@ Template.user_profile.events({
             birthDate: $("#input-bdate").val()
         };
         let uid = Template.instance().userId;
-        let u = User.findOne( {_id:uid} );
+        let u = User.findOne({
+            _id: uid
+        });
         if (u) {
             u.profileUpdate(uprofile);
         }
     },
-    'click button.btn-cancel'(event, instance) {
+    'click button.btn-cancel' (event, instance) {
         let $t = $(event.target);
         $t.closest(".container").find(".changed").removeClass("changed");
         $("#frm-profile")[0].reset();
     },
+    'click button.btn-email-change' (event, instance) {
+        updateEmailVisibility();
+        let $t = $(event.target);
+        $t.addClass('changed');
+        $("#btn-email-group").fadeIn();
+
+    },
+    'click button.email-save' (event, instance) {
+        let $t = $(event.target);
+        $t.closest(".container").find(".changed").removeClass("changed");
+        //todo: update database
+        let uid = Template.instance().userId;
+        let u = User.findOne({
+            _id: uid
+        });
+        if (u) {
+            u.profileUpdate(uprofile.emailAddress());
+        }
+    },
+    'click button.cancel-email' (event, instance) {
+        let $t = $(event.target);
+        $t.closest(".container").find(".changed").removeClass("changed");
+        $("#frm-profile")[0].reset();
+    }
 });
